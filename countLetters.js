8b781b2585121d2130b6217efce1b7711abb1899
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-24 23:22:34 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const countLetters = function(stringToCount) {
+  const noSpaces = stringToCount.split(" ").join("");
+  // Remove spaces from the input string because they cannot be counter
+
+  // Open an empty object to store the individual letter counts
+  const savedLetters = {};
+
+  for (const letters of noSpaces) {
+    savedLetters[letters] = savedLetters[letters] ? savedLetters[letters] + 1 : 1;
+    // If the current letter already exists in the object, increment its count by 1
+    // Otherwise, set its count to 1
+  }
+
+  return savedLetters; // Return the object with letter counts
+};
+
+//repeats until through all letters in the string NOT in alphabet
+
+const result = countLetters("lighthouse in the house"); // Call the countLetters function with the input string
+console.log(result); // Print the resulting object with letter counts
