de5c36ea042e7c59c1f8ac61166fda528217730b
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-25 00:07:55 +0000

    added new file

---
+++ findKeyByValue.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+
+  // if (actual === expected) {
+  //   console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  //   return;
+  // }
+  // console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+
+};
