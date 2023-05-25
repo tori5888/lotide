e28bce90d0988e06904cad8af75f075adad8a28b
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-25 22:21:10 +0000

    start

---
+++ map.js
@@ -0,0 +1,11 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function() {
+  const results = [];
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
