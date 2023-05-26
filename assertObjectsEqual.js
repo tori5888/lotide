64fdd326261e2b0e0c2f56a9d3b7d81c845476bd
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-26 15:18:13 +0000

    started by pasting eqObjects function in

---
+++ assertObjectsEqual.js
@@ -0,0 +1,34 @@
+//function to compare the two arrays for equality
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) { // check if arrays are diff length
+    return false;
+  }
+
+  //compares each element of the arrays
+  return arr1.every((element, index) => element === arr2[index]);
+};
+
+//func to assert equality of actual & expected values
+const assertEqual = function(actual, expected) {
+  //determines the assertion result message based on actual and expected values
+  const assertionMessage = actual === expected ? '✅ ✅ ✅ Assertion Passed' : '❌ ❌ ❌ Assertion Failed';
+  console.log(`${assertionMessage}: ${actual} === ${expected}`); //print
+};
+
+//compares two objects for equality
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {   //check if  objects have diff # of keys
+    return false;
+  }
+
+  //iterates over keys of first object
+  for (let key of Object.keys(object1)) {
+    //checks if both values for current key are arrays and if they don't match using eqArrays function
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key]) && !eqArrays(object1[key], object2[key])) {
+      return false;
+    }
+  }
+
+//happy path if all checks pass
+  return true;
+};
