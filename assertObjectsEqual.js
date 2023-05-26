5d6dd68a2b0b36dd9243f0fe47a8e0c8348e3636
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-26 15:52:35 +0000

    refractoring, notes add

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,47 +1,45 @@
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return false;
+    return false; //if arrays have diff lengths, they are not equal
   }
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false;
+      return false; //if any corresponding elements are not same the arrays are not equal
     }
   }
 
-  return true;
+  return true; // happy path all are equal
 };
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
+    return false; //if objects have different numbers of keys, they are not equal
   }
 
   for (const key of Object.keys(object1)) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
-        return false;
+        return false; // if values of a key are arrays, compare them using eqArrays
       }
     }
     if (object1[key] !== object2[key]) {
-      return false;
+      return false; //if values of a key are not equal -> objects are not equal
     }
   }
 
-  return true;
+  return true; //all keys and values are equal -> so objects are equal
 };
-
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-    return;
+    return; //objects are equal -> assertion passed
   }
   console.log(`🛑 🛑 🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  //objects are not equal -> assertion failed
 };
-
-
 
 //TEST
 const ab = { a: "1", b: "2" };
