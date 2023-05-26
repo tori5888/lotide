1adcf8f081591402ae08b779269875bb4a9e6d53
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-26 15:45:33 +0000

    works now

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,80 @@
-//function to compare the two arrays for equality
+// // Function to compare two objects for equality
+// const eqObjects = function(object1, object2) {
+//   // Implementation of eqObjects function (assuming you already have it)
+//   // ...
+// };
+
+// // Function to assert equality of two objects
+// const assertObjectsEqual = function(actual, expected) {
+//   // Compare the objects using eqObjects function
+//   const objectsAreEqual = eqObjects(actual, expected);
+
+//   // Generate the assertion message
+//   const assertionMessage = objectsAreEqual ? '✅ ✅ ✅ Objects are equal' : '❌ ❌ ❌ Objects are not equal';
+
+//   // Print the assertion message
+//   console.log(assertionMessage);
+// };
+
+// // Example usage
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { b: 2, a: 1 };
+// const obj3 = { a: 1, b: 3 };
+
+// assertObjectsEqual(obj1, obj2); // Output: ✅ ✅ ✅ Objects are equal
+// assertObjectsEqual(obj1, obj3); // Output: ❌ ❌ ❌ Objects are not equal
+
+
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // check if arrays are diff length
+  if (arr1.length !== arr2.length) {
     return false;
   }
 
-  //compares each element of the arrays
-  return arr1.every((element, index) => element === arr2[index]);
-};
-
-//func to assert equality of actual & expected values
-const assertEqual = function(actual, expected) {
-  //determines the assertion result message based on actual and expected values
-  const assertionMessage = actual === expected ? '✅ ✅ ✅ Assertion Passed' : '❌ ❌ ❌ Assertion Failed';
-  console.log(`${assertionMessage}: ${actual} === ${expected}`); //print
-};
-
-//compares two objects for equality
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {   //check if  objects have diff # of keys
-    return false;
-  }
-
-  //iterates over keys of first object
-  for (let key of Object.keys(object1)) {
-    //checks if both values for current key are arrays and if they don't match using eqArrays function
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key]) && !eqArrays(object1[key], object2[key])) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
 
-//happy path if all checks pass
   return true;
 };
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return;
+  }
+  console.log(`🛑 🛑 🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+
+
+
+//TEST
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // should pass
+
+const cd = { c: "1", d: ["2", 3] };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // should fail
