9b516e81e6f62ab1c9a146075ed7d023c4702784
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-06-12 18:34:55 +0000

    refractor

--- eqArrays.js
+++ eqArrays.js
@@ -23,7 +23,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// Example usage:
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Expected output: "✅ Assertion Passed: true === true"
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // Expected output: "✅ Assertion Passed: false === false"
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // Expected output: "✅ Assertion Passed: false === false"
+module.exports = eqArrays;
