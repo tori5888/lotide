189c372febe89d24e68cb8a42b5a50c42efcc531
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-06-12 18:33:51 +0000

    refractor and added test file

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+
+
+const assertEqual = require('../assertEqual');
+
+const eqArrays = require('../eqArrays');
+
+
+// Example usage:
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Expected output: "✅ Assertion Passed: true === true"
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // Expected output: "✅ Assertion Passed: false === false"
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // Expected output: "✅ Assertion Passed: false === false"
