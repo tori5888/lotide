1f85c8d60f75fa1a3f509ae1ceb293b76b3779c8
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-07-21 20:53:21 +0000

    remove extra require

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require('../assertEqual');
-
 const findKey = function(object, callback) {
   for (let key in object) {
     if (callback(object[key])) {
--- flatten.js
+++ flatten.js
@@ -1,8 +1,11 @@
 function flatten(arr) {
   return arr.reduce((result, element) => {
     if (Array.isArray(element)) {
-      result.push(...element);
+      // If the current element is an array, then the flatten function is called recursively on that array
+      result = result.concat(flatten(element));
+
     } else {
+      // If the current element is not an array, it's just pushed to the result array.
       result.push(element);
     }
     return result;
