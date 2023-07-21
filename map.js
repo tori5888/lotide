63374600d76bd18564f3fc026d0bdd4918ecfe53
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-07-21 19:35:23 +0000

    delete unnecessary code

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -7,24 +5,6 @@ const map = function(array, callback) {
   }
   return results;
 };
-
-const assertArrayEquals = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    console.error("Arrays have different lengths");
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.error(`Arrays differ at index ${i}`);
-      return false;
-    }
-  }
-
-  console.log("Arrays are equal");
-  return true;
-};
-
 
 
 module.exports = map;
